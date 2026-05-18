e613cb585c2c7558e70b47c3322ef6a51bbcb6ba
Saif Ali <this.saif.email@gmail.com>
2026-05-18 16:12:59 +0530

    feat(types): add CaseStage, EventType enums

---
+++ packages/types/src/enums/case.enums.ts
@@ -0,0 +1,19 @@
+export enum CaseStage {
+  New = 'new',
+  Contacted = 'contacted',
+  Qualified = 'qualified',
+  Negotiation = 'negotiation',
+  Won = 'won',
+  Lost = 'lost',
+}
+
+export enum EventType {
+  StageChanged = 'stage_changed',
+  AttributeUpdated = 'attribute_updated',
+  NoteAdded = 'note_added',
+  AssignmentChanged = 'assignment_changed',
+  CriteriaRejected = 'criteria_rejected',
+  TriggerFailed = 'trigger_failed',
+  CaseCreated = 'case_created',
+  CaseMerged = 'case_merged',
+}
