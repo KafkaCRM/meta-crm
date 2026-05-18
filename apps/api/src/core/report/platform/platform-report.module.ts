6ca2a1d99337c1dd389f9789c83068672bd9c467
Saif Ali <this.saif.email@gmail.com>
2026-05-19 02:25:19 +0530

    chore: update apps/api/src/core/report/platform/platform-report.module.ts

--- apps/api/src/core/report/platform/platform-report.module.ts
+++ apps/api/src/core/report/platform/platform-report.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PlatformDatabaseModule } from '../../tenant/platform-database.module';
 import { PlatformReportController } from './platform-report.controller';
 import { PlatformReportService } from './platform-report.service';
 
 @Module({
-  imports: [PlatformDatabaseModule],
+  imports: [],
   controllers: [PlatformReportController],
   providers: [PlatformReportService],
 })
