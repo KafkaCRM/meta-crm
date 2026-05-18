e75832e70105929df9c36e112a03c6b0b0abe2f4
Saif Ali <this.saif.email@gmail.com>
2026-05-18 16:12:59 +0530

    feat(types): add Channel, Direction enums

---
+++ packages/types/src/enums/interaction.enums.ts
@@ -0,0 +1,15 @@
+export enum Channel {
+  Email = 'email',
+  Sms = 'sms',
+  WhatsApp = 'whatsapp',
+  Call = 'call',
+  WebForm = 'web_form',
+  Facebook = 'facebook',
+  JustDial = 'justdial',
+  Note = 'note',
+}
+
+export enum Direction {
+  Inbound = 'inbound',
+  Outbound = 'outbound',
+}
