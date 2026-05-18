b8490be50ab39b7f6eefa48edb83df2ac6d76468
Saif Ali <this.saif.email@gmail.com>
2026-05-18 16:13:11 +0530

    feat(types): add Party DTO types

---
+++ packages/types/src/dtos/party.dto.ts
@@ -0,0 +1,6 @@
+import type { CreateParty, UpdateParty, PartyResponse, CheckDuplicateResponse } from '../schemas';
+
+export type CreatePartyDto = CreateParty;
+export type UpdatePartyDto = UpdateParty;
+export type PartyDto = PartyResponse;
+export type CheckDuplicateDto = CheckDuplicateResponse;
