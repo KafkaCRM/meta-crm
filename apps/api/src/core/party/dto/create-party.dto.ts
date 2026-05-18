825badecde47d11ceee3801229c2acf1e699bdb6
Saif Ali <this.saif.email@gmail.com>
2026-05-18 17:05:16 +0530

    feat(api): add CreatePartyDto

---
+++ apps/api/src/core/party/dto/create-party.dto.ts
@@ -0,0 +1,30 @@
+import { IsString, IsOptional, IsEmail, IsIn } from 'class-validator';
+import { PartyType, PartySource } from '@meta-crm/types';
+
+export class CreatePartyDto {
+  @IsString()
+  @IsIn([PartyType.Individual, PartyType.Organization])
+  type!: string;
+
+  @IsString()
+  name!: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsString()
+  branch_brand_assignment_id!: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsString()
+  @IsIn(Object.values(PartySource))
+  source?: string;
+
+  @IsOptional()
+  attributes?: Record<string, unknown>;
+}
