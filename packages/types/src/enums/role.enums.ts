dd14ac73f9175454c3e5b08c7c9a730a756c7f76
Saif Ali <this.saif.email@gmail.com>
2026-05-18 16:13:00 +0530

    feat(types): add TenantRole, PlatformRole enums

---
+++ packages/types/src/enums/role.enums.ts
@@ -0,0 +1,17 @@
+export enum TenantRole {
+  BranchUser = 'branch_user',
+  BranchManager = 'branch_manager',
+  BranchSupervisor = 'branch_supervisor',
+  TenantAdmin = 'tenant_admin',
+  TenantOwner = 'tenant_owner',
+}
+
+export enum PlatformRole {
+  PlatformOwner = 'platform_owner',
+  PlatformAdmin = 'platform_admin',
+  PlatformSupport = 'platform_support',
+  PlatformSales = 'platform_sales',
+  PlatformBilling = 'platform_billing',
+  PlatformDeveloper = 'platform_developer',
+  PlatformOps = 'platform_ops',
+}
