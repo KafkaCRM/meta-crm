808dd7e71d860e35e62810604900cc651a3467c5
Saif Ali <this.saif.email@gmail.com>
2026-05-18 19:14:49 +0530

    feat(web): add shared components barrel exports

---
+++ apps/web/src/components/shared/index.ts
@@ -0,0 +1,9 @@
+export { DynamicForm } from './DynamicForm';
+export type { FieldDefinition, DuplicateCheckResult } from './DynamicForm';
+
+export { VirtualTable } from './VirtualTable';
+
+export { NotificationToast } from './NotificationToast';
+
+export { BulkActionBar } from './BulkActionBar';
+export type { BulkAction } from './BulkActionBar';
