27c6711d00d43e8800bb11097a5ff7a9826a2664
Saif Ali <this.saif.email@gmail.com>
2026-05-18 16:13:01 +0530

    feat(types): add enums barrel export

---
+++ packages/types/src/enums/index.ts
@@ -0,0 +1,4 @@
+export { PartyType, PartySource, MergeStatus } from './party.enums';
+export { CaseStage, EventType } from './case.enums';
+export { Channel, Direction } from './interaction.enums';
+export { TenantRole, PlatformRole } from './role.enums';
