166f46aabe603abd534129318105fc09272861fc
Saif Ali <this.saif.email@gmail.com>
2026-05-18 16:13:12 +0530

    feat(types): add Interaction DTO types

---
+++ packages/types/src/dtos/interaction.dto.ts
@@ -0,0 +1,11 @@
+import type {
+  CreateInteraction,
+  UpdateInteraction,
+  InteractionResponse,
+  ThreadResponse,
+} from '../schemas';
+
+export type CreateInteractionDto = CreateInteraction;
+export type UpdateInteractionDto = UpdateInteraction;
+export type InteractionDto = InteractionResponse;
+export type ThreadDto = ThreadResponse;
