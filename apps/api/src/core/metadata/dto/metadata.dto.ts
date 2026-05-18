47af45feb702fb3e8ada86cabf8c9eff9007e82b
Saif Ali <this.saif.email@gmail.com>
2026-05-18 17:21:50 +0530

    feat(api): add metadata DTOs (CreateFieldDefinition, UpdateFieldDefinition, SetLabel)

---
+++ apps/api/src/core/metadata/dto/metadata.dto.ts
@@ -0,0 +1,60 @@
+import { IsString, IsOptional, IsIn, IsBoolean, IsNumber, IsArray, IsObject } from 'class-validator';
+
+export class CreateFieldDefinitionDto {
+  @IsString()
+  @IsIn(['Party', 'Case', 'Interaction'])
+  entity_type!: string;
+
+  @IsString()
+  name!: string;
+
+  @IsString()
+  label!: string;
+
+  @IsString()
+  @IsIn(['text', 'number', 'date', 'select', 'multi_select', 'boolean', 'phone', 'email'])
+  field_type!: string;
+
+  @IsOptional()
+  @IsArray()
+  options?: string[];
+
+  @IsOptional()
+  @IsBoolean()
+  required?: boolean;
+
+  @IsOptional()
+  @IsNumber()
+  order?: number;
+
+  @IsOptional()
+  @IsArray()
+  visibility_rules?: unknown[];
+}
+
+export class UpdateFieldDefinitionDto {
+  @IsOptional()
+  @IsString()
+  label?: string;
+
+  @IsOptional()
+  @IsArray()
+  options?: string[];
+
+  @IsOptional()
+  @IsBoolean()
+  required?: boolean;
+
+  @IsOptional()
+  @IsNumber()
+  order?: number;
+
+  @IsOptional()
+  @IsArray()
+  visibility_rules?: unknown[];
+}
+
+export class SetLabelDto {
+  @IsString()
+  value!: string;
+}
