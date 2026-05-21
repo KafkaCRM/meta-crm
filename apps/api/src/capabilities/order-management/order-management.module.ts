88f005298b80be30cf7c7e8c60d2fe570389f555
Saif Ali <this.saif.email@gmail.com>
2026-05-22 04:22:18 +0530

    feat: add apps/api/src/capabilities/order-management/order-management.module.ts for tenant capabilities framework

---
+++ apps/api/src/capabilities/order-management/order-management.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { OrderManagementController } from './order-management.controller';
+import { OrderManagementService } from './order-management.service';
+
+@Module({
+  imports: [],
+  controllers: [OrderManagementController],
+  providers: [OrderManagementService],
+  exports: [OrderManagementService],
+})
+export class OrderManagementModule {}
