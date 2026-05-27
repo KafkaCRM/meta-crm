1d631679e8a63d3e222839fd28f03c25a8e87570
Saif Ali <this.saif.email@gmail.com>
2026-05-27 07:40:57 +0530

    refactor(api): register bull queue processor for automation flow module

--- apps/api/src/core/workflow/workflow.module.ts
+++ apps/api/src/core/workflow/workflow.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bullmq';
 import { WorkflowController } from './workflow.controller';
 import { WorkflowService } from './workflow.service';
+import { AutomationFlowController } from './automation-flow.controller';
+import { AutomationFlowService } from './automation-flow.service';
+import { AutomationFlowListener } from './automation-flow.listener';
+import { AutomationFlowProcessor } from './automation-flow.processor';
+import { WhatsAppModule } from '../../integrations/whatsapp/whatsapp.module';
 
 @Module({
-  controllers: [WorkflowController],
-  providers: [WorkflowService],
-  exports: [WorkflowService],
+  imports: [
+    WhatsAppModule,
+    BullModule.registerQueue({
+      name: 'workflow',
+    }),
+  ],
+  controllers: [WorkflowController, AutomationFlowController],
+  providers: [
+    WorkflowService,
+    AutomationFlowService,
+    AutomationFlowListener,
+    AutomationFlowProcessor,
+  ],
+  exports: [
+    WorkflowService,
+    AutomationFlowService,
+  ],
 })
 export class WorkflowModule {}
