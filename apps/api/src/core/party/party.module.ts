4b6c0a96b07742b9997b4a24b6fb75f30addcc2e
Saif Ali <this.saif.email@gmail.com>
2026-05-27 07:40:55 +0530

    refactor(api): inject database context in party manager module

--- apps/api/src/core/party/party.module.ts
+++ apps/api/src/core/party/party.module.ts
@@ -4,9 +4,10 @@ import { PartyService } from './party.service';
 import { PartyUpsertService } from './party-upsert.service';
 import { PartyMergeService } from './party-merge.service';
 import { MetadataModule } from '../metadata/metadata.module';
+import { HooksModule } from '../hooks/hooks.module';
 
 @Module({
-  imports: [MetadataModule],
+  imports: [MetadataModule, HooksModule],
   controllers: [PartyController],
   providers: [PartyService, PartyUpsertService, PartyMergeService],
   exports: [PartyService, PartyUpsertService, PartyMergeService],
